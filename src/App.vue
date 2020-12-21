6cb3d7dbb74cab455725355af2c5918a08ac2660
Martha <marthaceciliaortizmendez@gmail.com>
2020-12-21 17:08:29 -0500

    se hicieron modificaciones de algunas tablas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
- <v-app id="inspire">
+  <v-app id="inspire">
     <v-system-bar app>
       <v-spacer></v-spacer>
 
@@ -13,7 +13,7 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Admin Vitapets</v-toolbar-title>
     </v-app-bar>
 
     <v-navigation-drawer
@@ -21,15 +21,82 @@
       fixed
       temporary
     >
+    <v-card
+      class="mx-auto"
+      width="300"
+    >
+      <v-list>
+        <v-list-item>
+          to="Home"
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+  
+          <v-list-item-title>Inicio</v-list-item-title>
+        </v-list-item>
+  
+        <v-list-group
+          :value="true"
+          prepend-icon="mdi-account-circle"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Users</v-list-item-title>
+          </template>
+  
+          <v-list-group
+            :value="true"
+            no-action
+            sub-group
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Admin</v-list-item-title>
+              </v-list-item-content>
+            </template>
+  
+            <v-list-item
+              v-for="([title, icon], i) in admins"
+              :key="i"
+              link
+            >
+              <v-list-item-title v-text="title"></v-list-item-title>
+  
+              <v-list-item-icon>
+                <v-icon v-text="icon"></v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list-group>
+  
+          <v-list-group
+            no-action
+            sub-group
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Actions</v-list-item-title>
+              </v-list-item-content>
+            </template>
+  
+            <v-list-item
+              v-for="([title, icon, ruta], i) in cruds"
+              :key="i"
+              :to="ruta"
+              link
+            >
+              <v-list-item-title v-text="title"></v-list-item-title>
+  
+              <v-list-item-icon>
+                <v-icon v-text="icon"></v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list-group>
+        </v-list-group>
+      </v-list>
+    </v-card>
       <!--  -->
     </v-navigation-drawer>
 
     <v-main class="grey lighten-2">
-      <v-container fluid>
-
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
       <v-container>
         <v-row>
           <template v-for="n in 4">
@@ -54,7 +121,6 @@
       </v-container>
     </v-main>
   </v-app>
-
 </template>
 
 <script>
@@ -71,6 +137,14 @@ export default {
   data: () => ({
     //
     drawer: null}),
+    admins: [
+      ['Usuarios', 'mdi-account-multiple-outline'],
+      ['Configuracion', 'mdi-cog-outline'],
+    ],
+    cruds: [
+      ['Articulos', 'mdi-plus-outline', 'Articulos'],
+      ['Categorias', 'mdi-view-list', 'Categorias'],
+    ],
 };
 
 </script>
