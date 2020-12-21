100f3e84541d152eeb03208b45229d54a81996fd
Martha <marthaceciliaortizmendez@gmail.com>
2020-12-21 17:43:18 -0500

    cambios varios

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/Home',
     name: 'Home',
     component: Home
   },
@@ -19,12 +19,12 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/tabla',
-    name: 'Tabla',
+    path: '/categorias',
+    name: 'Categorias',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Tabla.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Categorias.vue')
   }
 ]
 
