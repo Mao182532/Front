37d26e94c99ac20286aa935b5724c5df786c1f15
OmarPerez03 <omar.andres.03.06@hotmail.com>
2020-12-22 17:03:21 -0500

    cargue menu

--- src/App.vue
+++ src/App.vue
@@ -17,85 +17,83 @@
     </v-app-bar>
 
     <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      temporary
-      
-    >
+        v-model="drawer"
+        fixed
+        temporary
+        
+      >
     <v-card
-      class="mx-auto"
-      width="300"
-    >
-      <v-list>
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-  
-          <v-list-item-title>Inicio</v-list-item-title>
-        </v-list-item>
-  
-        <v-list-group
-          :value="true"
-          prepend-icon="mdi-account-circle"
-        >
-          <template v-slot:activator>
-            <v-list-item-title>Users</v-list-item-title>
-          </template>
-  
+        class="mx-auto"
+        width="300"
+      >
+        <v-list>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+    
+            <v-list-item-title>Menu</v-list-item-title>
+          </v-list-item>
+    
           <v-list-group
             :value="true"
-            no-action
-            sub-group
+            prepend-icon="mdi-account-circle"
           >
             <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>Admin</v-list-item-title>
-              </v-list-item-content>
+              <v-list-item-title>Users</v-list-item-title>
             </template>
-  
-            <v-list-item
-              v-for="([title, icon], i) in admins"
-              :key="i"
-              link
+    
+            <v-list-group
+              :value="true"
+              no-action
+              sub-group
             >
-              <v-list-item-title v-text="title"></v-list-item-title>
-  
-              <v-list-item-icon>
-                <v-icon v-text="icon"></v-icon>
-              </v-list-item-icon>
-            </v-list-item>
-          </v-list-group>
-  
-          <v-list-group
-            no-action
-            sub-group
-          >
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>Actions</v-list-item-title>
-              </v-list-item-content>
-            </template>
-  
-            <v-list-item
-              v-for="([title, icon,ruta], i) in cruds"
-              :key="i"
-              :to="ruta"
-              link
-
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>Admin</v-list-item-title>
+                </v-list-item-content>
+              </template>
+    
+              <v-list-item
+                v-for="([title, icon], i) in admins"
+                :key="i"
+                link
+              >
+                <v-list-item-title v-text="title"></v-list-item-title>
+    
+                <v-list-item-icon>
+                  <v-icon v-text="icon"></v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+            </v-list-group>
+    
+            <v-list-group
+              no-action
+              sub-group
             >
-              <v-list-item-title v-text="title"></v-list-item-title>
-  
-              <v-list-item-icon>
-                <v-icon v-text="icon"></v-icon>
-              </v-list-item-icon>
-            </v-list-item>
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>Opciones</v-list-item-title>
+                </v-list-item-content>
+              </template>
+    
+              <v-list-item
+                v-for="([title, icon, ruta], i) in cruds"
+                :key="i"
+                :to="ruta"
+                link
+              >
+                <v-list-item-title v-text="title"></v-list-item-title>
+    
+                <v-list-item-icon>
+                  <v-icon v-text="icon"></v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+            </v-list-group>
           </v-list-group>
-        </v-list-group>
-      </v-list>
-    </v-card>
-      <!--  -->
-    </v-navigation-drawer>
+        </v-list>
+      </v-card>
+     </v-navigation-drawer>
 
     <v-main class="grey lighten-2">
       <v-container fluid>
@@ -118,14 +116,16 @@ export default {
   },
 
   data: () => ({
-    draw:null,
-   admins: [
-      ['Usuarios', 'mdi-account-multiple-outline'],
-      ['Configuracion', 'mdi-cog-outline'],
+    drawer:null,
+    admins: [
+      ['Management', 'mdi-account-multiple-outline'],
+      ['Settings', 'mdi-cog-outline'],
     ],
     cruds: [
-      ['Articulos', 'mdi-plus-outline','Articulos'],
-      ['Categoria', 'mdi-file-outline','Categorias'],],})
-    };
+      ['Articulo', 'mdi-plus-outline','Articulos'],
+      ['Categoria', 'mdi-file-outline','Categorias'],
+    ],
+  })
+  };
 
 </script>
